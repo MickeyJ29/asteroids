8d1ed4e329a6533f99f50d321f15c2dd8a18b500
MickeyJ <michaeljconnolly1990@gmail.com>
2025-10-21 23:36:24 +1100

    Triangle now rotates

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
+
 def main():
     print("Starting Asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while 1==1:
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
